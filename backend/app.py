6cd56e187b141262398553242062c7e07012340b
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2024-02-14 09:39:48 -0800

    build(deps): update app.py and requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,20 @@ from flask import Flask, jsonify, send_from_directory
 from dotenv import load_dotenv
 from flask import request
 from flask_cors import CORS
+import os
+import openai as OpenAI
 
 load_dotenv()
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
